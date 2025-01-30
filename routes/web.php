4e66056d8d798a84077c9c621616c14180bba57c
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-01-30 22:38:26 +0000

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ResumeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/upload-resume', [ResumeController::class, 'uplaodResume'])->name('upload-resume');
+Route::post('/upload-resume', [ResumeController::class, 'uploadResume'])->name('upload-resume');
